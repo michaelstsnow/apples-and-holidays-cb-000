a1ab365fb8e94be4a39a7d7402672ed71ae8a840
michaelstsnow <michael.st.snow@gmail.com>
2018-11-11 21:48:56 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -85,7 +85,7 @@ def all_supplies_in_holidays(holiday_hash)
       supplies.each do |supply|
         t_supply = supply.slice(0,1).capitalize + supply.slice(1..-1)
         if counter == 0
-          #all_supplies = t_supply
+          all_supplies = t_supply
           counter = 1
         else
           all_supplies = all_supplies + ", " + t_supply
