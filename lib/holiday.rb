d0a04ad3d3d90d48e377b254b8fbe23d23c482b8
michaelstsnow <michael.st.snow@gmail.com>
2018-11-11 21:32:41 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,7 +75,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |season,holidays|
     tseason = season.to_s
-    puts "#{tseason}"
+    puts "#{tseason}:"
   end
 end
 
